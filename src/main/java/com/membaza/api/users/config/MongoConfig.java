09d2db9fbf206796444b3b943648205388688afd
pyknic <emil@speedment.com>
2017-04-01 20:12:45 -0700

    Load MongoDB database name from properties

--- src/main/java/com/membaza/api/users/config/MongoConfig.java
+++ src/main/java/com/membaza/api/users/config/MongoConfig.java
@@ -1,8 +1,10 @@
 package com.membaza.api.users.config;
 
 import com.mongodb.Mongo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,9 +16,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class MongoConfig {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     public MongoTemplate mongoTemplate(Mongo mongo) throws Exception {
-        return new MongoTemplate(mongo, "membaza-users");
+        return new MongoTemplate(
+            mongo, env.getProperty("spring.data.mongodb.database")
+        );
     }
 
     @Bean
